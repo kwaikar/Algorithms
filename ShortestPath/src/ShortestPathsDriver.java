7d8750f8ced1a6405387d87f6c389ca1b84ff804
kwaikar <kanchan@rolloutaec.com>
2016-03-27 10:33:51 -0500

    adding changes

--- ShortestPath/src/ShortestPathsDriver.java
+++ ShortestPath/src/ShortestPathsDriver.java
@@ -1,20 +1,46 @@
+import java.util.HashSet;
+import java.util.List;
 
 public class ShortestPathsDriver {
 
 	public static void main(String[] args) {
 
 		Graph graph = Graph.acceptGraphInput(args[0], GraphType.DIRECTED);
-		Vertex root = graph.getVerts().get(0);
-		int weight=graph.getVerts().get(0).getAdj().get(0).getWeight();
-		boolean hasUniformEdges =false;
+		int weight = graph.getVerts().get(0).getAdj().get(0).getWeight();
+		boolean hasUniformEdges = true;
+		boolean negativeWeights = false;
+		boolean isCyclic = false;
 		for (Vertex vertex : graph) {
-			
-			for (Edge edge: vertex.getAdj()) {
-				 
-				
+
+			for (Edge edge : vertex.getAdj()) {
+
+				if (hasUniformEdges && edge.getWeight() != weight) {
+					hasUniformEdges = false;
+				}
+				if (!negativeWeights && edge.getWeight() < 0) {
+					negativeWeights = true;
+				}
+			}
+			if (TopologicalOrdering.isCyclic(vertex, new HashSet<Vertex>())) {
+				isCyclic = true;
 			}
 		}
 		
+		List<Edge> edges = null;
+		if (hasUniformEdges && !negativeWeights) {
+			BreadthFirstSearch bfs = new BreadthFirstSearch();
+			edges = bfs.shortestPath();
+		} else if (!negativeWeights && !isCyclic) {
+			DAGShortest dag = new DAGShortest();
+			edges = dag.shortestPath();
+		} else if (!negativeWeights && isCyclic) {
+			Dijkstra dijkstra = new Dijkstra();
+			edges = dijkstra.shortestPath();
+		} else {
+			BellmanFord bf = new BellmanFord();
+			edges = bf.getShortestPath(graph);
+		}
+
 		for (Vertex vertex : graph) {
 			vertex.setSeen(false);
 		}
