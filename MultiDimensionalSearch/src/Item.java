70594336675255d6dacebfb4bbeef5c780260331
kwaikar <kanchan@rolloutaec.com>
2016-04-04 17:23:06 -0500

    checking in code

--- MultiDimensionalSearch/src/Item.java
+++ MultiDimensionalSearch/src/Item.java
@@ -1,14 +1,26 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * This is a pojo for Item class.
  * 
- * @author Kanchan Waikar Date Created : Apr 3, 2016 - 10:27:15 PM
+ * @author Kavya Krishna Maringanty, Kanchan Waikar, Sruti Paku
+ * 
+ *         Date Created : Apr 3, 2016 - 10:27:15 PM
  *
  */
 public class Item {
 	private long id;
-	private double price;
-	private long[] description;
+	private Double price;
+	private Long[] sortedDescription;
+
+	public Item(long id, Double price, Long[] sortedDescription) {
+		super();
+		this.id = id;
+		this.price = price;
+		this.sortedDescription = sortedDescription;
+	}
 
 	/**
 	 * @return the id
@@ -28,7 +40,7 @@ public class Item {
 	/**
 	 * @return the price
 	 */
-	public double getPrice() {
+	public Double getPrice() {
 		return price;
 	}
 
@@ -43,16 +55,49 @@ public class Item {
 	/**
 	 * @return the description
 	 */
-	public long[] getDescription() {
-		return description;
+	public Long[] getDescription() {
+		return sortedDescription;
 	}
 
 	/**
 	 * @param description
 	 *            the description to set
 	 */
-	public void setDescription(long[] description) {
-		this.description = description;
+	public void setDescription(Long[] description) {
+		  Collections.sort(Arrays.asList(description));
+		  
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (id ^ (id >>> 32));
+		return result;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Item other = (Item) obj;
+		if (id != other.id)
+			return false;
+		return true;
 	}
 
 }
--- MultiDimensionalSearch/src/MultiDimensionalSearch.java
+++ MultiDimensionalSearch/src/MultiDimensionalSearch.java
@@ -1,43 +1,143 @@
 import java.util.Comparator;
-import java.util.PriorityQueue;
-import java.util.Queue;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+import java.util.TreeSet;
 
 /**
  * This class implements Multi-dimensional Search
  * 
- * @author Kanchan Waikar Date Created : Apr 3, 2016 - 7:08:23 PM
+ * @author Kavya Krishna Maringanty, Kanchan Waikar, Sruti Paku
+ * 
+ *         Date Created : Apr 3, 2016 - 7:08:23 PM
  *
  */
 public class MultiDimensionalSearch {
-	
-	
-	int insert(long id, double price, long[] description, int size) {
+
+	private final Set EMPTY_SET = new HashSet();
+
+	Map<Long, TreeSet<Item>> mapOfDescriptionSubStringAndPrice = new TreeMap<Long, TreeSet<Item>>();
+	Map<Long, Item> mapById = new HashMap<Long, Item>();
+
+	int insert(Long id, double price, Long[] description, int size) {
 		// Description of item is in description[0..size-1].
 		// Copy them into your data structure.
-		return 0;
+		Item item = mapById.get(id);
+		boolean isNew = false;
+
+		Long[] oldDescription = null;
+
+		if (item == null) {
+			item = new Item(id, price, description);
+			isNew = true;
+		} else {
+			oldDescription = item.getDescription();
+			if (description.length == 0) {
+				description = item.getDescription();
+				item.setPrice(price);
+			} else {
+				item.setDescription(description);
+				item.setPrice(price);
+			}
+		}
+		mapById.put(item.getId(), item);
+
+		for (long subString : description) {
+			TreeSet<Item> treeSet = mapOfDescriptionSubStringAndPrice.get(description[0]);
+			if (treeSet == null) {
+				treeSet = new TreeSet<>(new Comparator<Item>() {
+					@Override
+					public int compare(Item o1, Item o2) {
+						return o1.getPrice().compareTo(o2.getPrice());
+					}
+				});
+			}
+			treeSet.add(item);
+			mapOfDescriptionSubStringAndPrice.put(subString, treeSet);
+		}
+		if(oldDescription!=null)
+		{
+			for (long subString : description) {
+				TreeSet<Item> treeSet = mapOfDescriptionSubStringAndPrice.get(subString);
+				if (treeSet != null) {
+					 treeSet.remove(item);
+				}
+			}	
+		}
+		 
+		if (isNew) {
+			return 1;
+		} else {
+			return 0;
+		}
 	}
 
 	double find(long id) {
-		return 0;
+		Item item = mapById.get(id);
+		if (item == null) {
+			return 0;
+		} else {
+			return item.getPrice();
+		}
 	}
 
 	long delete(long id) {
+		Item item = mapById.get(id);
+
+		for (long subString : item.getDescription()) {
+			TreeSet<Item> set = mapOfDescriptionSubStringAndPrice.get(subString);
+			Item itemTobeDeleted = null;
+			for (Item item2 : set) {
+				if (item2.getId() == id) {
+					itemTobeDeleted = item2;
+				}
+			}
+			set.remove(itemTobeDeleted);
+		}
+
 		return 0;
 	}
 
 	double findMinPrice(long des) {
-		return 0;
+		TreeSet<Item> set = mapOfDescriptionSubStringAndPrice.get(des);
+		if (set != null && set.size() != 0) {
+			set.first().getPrice();
+		}
+		return Double.MIN_VALUE;
 	}
 
 	double findMaxPrice(long des) {
-		return 0;
+		TreeSet<Item> set = mapOfDescriptionSubStringAndPrice.get(des);
+		if (set != null && set.size() != 0) {
+
+			set.last().getPrice();
+		}
+		return Double.MIN_VALUE;
 	}
 
 	int findPriceRange(long des, double lowPrice, double highPrice) {
-		return 0;
+		return extractSubSetForDescription(des, lowPrice, highPrice).size();
+	}
+
+	/**
+	 * @param des
+	 * @param lowPrice
+	 * @param highPrice
+	 */
+	private Set<Item> extractSubSetForDescription(long des, double lowPrice, double highPrice) {
+		TreeSet<Item> set = mapOfDescriptionSubStringAndPrice.get(des);
+		if (set != null && set.size() != 0) {
+			Item lowItem = new Item(-1L, lowPrice, null);
+			Item highItem = new Item(-1L, highPrice, null);
+			return set.subSet(lowItem, true, highItem, true);
+		}
+		return EMPTY_SET;
 	}
 
 	double priceHike(long minid, long maxid, double rate) {
+
 		return 0;
 	}
 
